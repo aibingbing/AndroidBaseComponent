a076a9b670fd0dcc4d6dae90789099fcef507000
aibingbing <aibingbing1991@qq.com>
2020-09-06 22:50:29 +0800

    refactor:kotin_version 1.3.61 -> 1.4.0, gradle 5.6.4 -> 6.1.1, gradle-plugin 3.6.4 -> 4.0.1 feat:navigation-safe-arg-gradle-version:2.2.0 refactor:暂时去掉novada-bintray的发布插件，因为和gradle-plugin:4.0.1冲突； feat:添加BaseMVVM，和ArchitecturePractice_Sunflower 模块； refactor:升级app模块中的dependencies version;

---
+++ ArchitecturePractice_Sunflower/src/main/java/com/aibb/android/demo/sunflower/GardenActivity.kt
@@ -0,0 +1,17 @@
+package com.aibb.android.demo.sunflower
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright:   Copyright (c)  All rights reserved.<br>
+ * Author:      aibingbing <br>
+ * Date:        2020/9/6 <br>
+ * Desc:        <br>
+ */
+class GardenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_garden)
+    }
+}
---
+++ ArchitecturePractice_Sunflower/src/main/java/com/aibb/android/demo/sunflower/SunflowerApplication.kt
@@ -0,0 +1,15 @@
+package com.aibb.android.demo.sunflower
+
+import android.app.Application
+
+/**
+ * Copyright:   Copyright (c)  All rights reserved.<br>
+ * Author:      aibingbing <br>
+ * Date:        2020/9/6 <br>
+ * Desc:        <br>
+ */
+class SunflowerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
